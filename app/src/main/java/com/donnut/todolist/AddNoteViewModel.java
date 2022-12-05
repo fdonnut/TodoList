304465c693a7b23da77b2783f7aee41a6f095df7
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-12-05 17:53:50 +0300

    disposable

--- app/src/main/java/com/donnut/todolist/AddNoteViewModel.java
+++ app/src/main/java/com/donnut/todolist/AddNoteViewModel.java
@@ -1,13 +1,18 @@
 package com.donnut.todolist;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -15,6 +20,7 @@ public class AddNoteViewModel extends AndroidViewModel {
 
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -26,14 +32,23 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void saveNote(Note note) {
-        notesDao.addNote(note)
+        Disposable disposable = notesDao.addNote(note)
+                .delay(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
             @Override
             public void run() throws Throwable {
+                Log.d("AddNoteViewModel", "subscribe");
                 shouldCloseScreen.setValue(true);
             }
         });
+        compositeDisposable.add(disposable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
