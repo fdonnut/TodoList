87da11c5d53ab8ce1e512af36c699e54558df42c
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-12-05 12:40:13 +0300

    MutableLiveData

--- app/src/main/java/com/donnut/todolist/MainActivity.java
+++ app/src/main/java/com/donnut/todolist/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,13 +29,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewModel = new MainViewModel(getApplication());
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer count) {
+                Toast.makeText(MainActivity.this, String.valueOf(count), Toast.LENGTH_SHORT).show();
+            }
+        });
         initViews();
 
         notesAdapter = new NotesAdapter();
         notesAdapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
             @Override
             public void onNoteClick(Note note) {
+                viewModel.showCount();
             }
         });
         recyclerViewNotes.setAdapter(notesAdapter);
--- app/src/main/java/com/donnut/todolist/MainViewModel.java
+++ app/src/main/java/com/donnut/todolist/MainViewModel.java
@@ -5,12 +5,15 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import java.util.List;
 
 class MainViewModel extends AndroidViewModel {
 
     private NoteDatabase noteDatabase;
+    private int count = 0;
+    private MutableLiveData<Integer> countLD = new MutableLiveData<>();
 
     public MainViewModel(@NonNull Application application) {
         super(application);
@@ -19,6 +22,15 @@ class MainViewModel extends AndroidViewModel {
 
     public LiveData<List<Note>> getNotes() {
         return noteDatabase.notesDao().getNotes();
+    }
+
+    public void showCount() {
+        count++;
+        countLD.setValue(count);
+    }
+
+    public LiveData<Integer> getCount() {
+        return countLD;
     }
 
     public void remove(Note note) {
